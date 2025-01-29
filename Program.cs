b3022c1345b8c07c178a023bd2a7e0064c139fe6
Leonardo Castagnaro <castagnaro.leonardo@gmail.com>
2025-01-29 23:32:36 +0100

    prova

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Raylib_cs;
+
+namespace RaylibGame;
+
+class Program
+{
+    public static void Main()
+    {
+        Raylib.InitWindow(1280, 720, "Hello World");
+        Raylib.SetTargetFPS(60);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
